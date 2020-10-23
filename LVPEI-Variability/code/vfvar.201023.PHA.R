832a646395939cb7ebd75e6a5c02d74d3ca2513d
phartes20 <paul.h.artes@gmail.com>
2020-10-23 17:17:20 +0100

    see comments

---
+++ LVPEI-Variability/code/vfvar.201023.PHA.R
@@ -0,0 +1,31 @@
+
+# work on Variability Index
+# Fabian Yii, Paul Artes
+
+# To do: make a scatterplot variability vs sensitivity using LVPEI data
+
+
+rm(list=ls())
+setwd("/Users/fabianyii/Desktop/fy.r/LVPEI-Variability/")
+# install.packages("devtools")
+# install_github("imarinfr/vf1/source")
+library(devtools)
+library("visualFields")
+
+lvdat    <- read.csv( "data/LVPFirst100Data_processed.2.csv", stringsAsFactors = FALSE )
+lvdat$date <- as.Date(lvdat$date, "%m/%d/%y")
+
+# for(i in 1:54){
+# names(lvdat)[i+11] <- i 
+# }
+
+plot_res <- unique(data.frame(eye = lvdat$eye, id = lvdat$id))
+
+for (i in 1:length(plot_res$id)) {
+  idx <- which(lvdat$id == plot_res$id[i] & lvdat$eye == plot_res$eye[i])
+  d <- data.frame( lvdat[idx[1:length(idx)],2:65] )
+  m <- d[,-c(1:10)]
+  var <- mean( apply( m, 2, sd) )
+  m.s <- mean( apply(m,2,mean) )
+  plot(m.s, var, ylim=c(-3,6))   # no, this doesn't make sense -- because each patient produces only a single point here.
+}                                # you need to plot outside the loop...  also: var is a reserved word - check ?var
