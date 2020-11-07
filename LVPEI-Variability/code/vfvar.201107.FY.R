65eeeb5fa39d69c7159928898958767f1734c98d
fyii200 <fslyii@hotmail.com>
2020-11-07 16:53:58 +0800

    201107

---
+++ LVPEI-Variability/code/vfvar.201107.FY.R
@@ -0,0 +1,49 @@
+
+# work on Variability Index
+# Fabian Yii, Paul Artes
+
+rm(list=ls())
+setwd("/Users/fabianyii/Desktop/fy.r/LVPEI-Variability/")
+
+# setwd("c:/Users/paul_/Google Drive/People_2/Fabian Yii/fy.r/LVPEI-Variability/")
+
+
+# install.packages("devtools")
+# library(devtools)
+# install_github("imarinfr/vf1/source")
+
+library( "visualFields" )
+
+lvdat    <- read.csv( "data/LVPFirst100Data_processed.2.csv", stringsAsFactors = FALSE )
+lvdat$date <- as.Date(lvdat$date, "%m/%d/%y")
+
+plot_res <- data.frame( id = unique( lvdat$id ), mean.s = 0, varb.s = 0)
+
+
+
+for (i in 1:length(plot_res$id)) {
+  idx <- plot_res$id[i]
+  
+  d <- subset (lvdat, id == idx, select = L1:L54 )
+  for (q in 1:length(d)) {
+    ifelse (d[q] == 0, d[q] <- NA, d[q] <- d[1:nrow(d),q])
+  }
+  
+  d.t <- subset (lvdat, id == idx, select = L1:L54 )
+  for (q in 1:length(d.t)) {
+    ifelse (d.t[q] == 0, d.t[q] <- NA, d.t[q] <- d.t[1:nrow(d.t),q])
+  }
+  
+  d.t<- 16.7764*(exp(0.078*d.t))
+  
+  plot_res$varb.s[i] <- mean( apply(d.t, 2, sd),na.rm = TRUE)
+  plot_res$mean.s[i] <- mean(apply (d, 2, mean),na.rm = TRUE)
+}
+  
+plot_res2 <- plot_res [which (plot_res$varb.s > 0.01),]
+
+pdf (file="plot.pdf", width=6, height=6)
+plot (plot_res2$mean.s, plot_res2$varb.s, main = "VF Variability vs Mean Sensitivity", 
+      col="blue", bty="n", pch=19, xlab="Mean Sensitivity (dB)", ylab="VF Variability (dB)",
+      xlim = c(-5,30), ylim = c(0,50) )
+dev.off()
